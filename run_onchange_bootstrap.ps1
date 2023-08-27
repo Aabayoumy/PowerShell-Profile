d9918f9e328d8bc894357d18a9266430a0504860
Ahmed Bayoumy <abayoumy@outlook.com>
2023-08-27 11:54:02 +0300

    bootstrap ps1

--- run_onchange_bootstrap.ps1
+++ run_onchange_bootstrap.ps1
@@ -9,6 +9,11 @@ if (-not (Get-Module -Name "z")) {
     Write-Host "Installing z" 
     Install-Module z -force }
 
+if (-not (Get-Module -Name "Microsoft.WinGet.Client")) {
+    Write-Host "Installing Microsoft.WinGet.Client" 
+    Install-Module Microsoft.WinGet.Client -force }
+   
+
 # Install Chocolatey
 if (!(Test-Path -Path "$env:ProgramData\Chocolatey")) {
     Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
