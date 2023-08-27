596a4f6126bfed74b5df0c0e29466fe014651d54
Ahmed Bayoumy <abayoumy@outlook.com>
2023-08-27 10:39:33 +0300

    Initial

---
+++ OneDrive - GLOBAL BRANDS/Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,35 @@
+
+# set PowerShell to UTF-8
+[console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
+
+Import-Module -Name Terminal-Icons
+Import-Module z
+
+
+# Alias
+Set-Alias -Name vim -Value nvim
+Set-Alias ll ls
+Set-Alias g git
+Set-Alias grep findstr
+
+if ($host.Name -eq 'ConsoleHost')
+{
+  Import-Module PSReadLine
+  # PSReadLine
+  Set-PSReadLineOption -EditMode Emacs
+  Set-PSReadLineOption -BellStyle None
+  
+  # Shows navigable menu of all options when hitting Tab
+  Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
+
+  # Autocompleteion for Arrow keys
+  Set-PSReadLineOption -HistorySearchCursorMovesToEnd
+  Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+  Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+
+  Set-PSReadLineOption -ShowToolTips
+  Set-PSReadLineOption -PredictionSource History
+
+}
+
+Invoke-Expression (&starship init powershell)
