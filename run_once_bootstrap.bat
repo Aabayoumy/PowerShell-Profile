63caa8eb6052468a4d9e6175ab9de53f09d77826
Ahmed Bayoumy <abayoumy@outlook.com>
2023-08-27 14:00:47 +0300

    update bootstrap

--- run_once_bootstrap.bat
+++ run_once_bootstrap.bat
@@ -1,4 +1,4 @@
 powershell -NoProfile  -ExecutionPolicy ByPass -Command "Uninstall-Module PowerShellGet"
 powershell -NoProfile  -ExecutionPolicy ByPass -Command "Install-Module -Name PowerShellGet -Force -AllowClobber -RequiredVersion 2.2.5"
 powershell -NoProfile  -ExecutionPolicy ByPass -Command "Install-Module -Name PSReadLine -Force "
-start /wait Powershell.exe -executionpolicy bypass -File  "c:\Scripts\start.ps1"
+start /wait Powershell.exe -executionpolicy bypass -File  "%userProfile%\Scripts\start.ps1"
