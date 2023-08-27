4dcd9997f825c168935ef7a3188bf71418ff3da6
Ahmed Bayoumy <abayoumy@outlook.com>
2023-08-27 14:05:02 +0300

    update start.ps1

--- Scripts/Start.ps1
+++ Scripts/Start.ps1
@@ -1,6 +1,6 @@
-$ScriptsCount = (Get-ChildItem | where-object {$_.Name -match "^[0-9][0-9]"}).count
+$ScriptsCount = (Get-ChildItem $env:USERPROFILE\Scripts | where-object {$_.Name -match "^[0-9][0-9]"}).count
 $i=0
-Get-ChildItem | where-object {$_.Name -match "^[0-9][0-9]"} | Sort-Object | ForEach-Object {
+Get-ChildItem $env:USERPROFILE\Scripts | where-object {$_.Name -match "^[0-9][0-9]"} | Sort-Object | ForEach-Object {
   $i++
   $host.UI.RawUI.WindowTitle = "$i of $ScriptsCount : $_"
   & $_.FullName
