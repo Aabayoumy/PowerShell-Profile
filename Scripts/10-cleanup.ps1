c337801d115cc9c86356c6013bdcd6d6308e2387
Ahmed M. Bayoumy <abayoumy@outlook.com>
2023-12-25 12:33:02 +0200

    Rename 10-cleanup.ps1 to cleanup.ps1

--- Scripts/10-cleanup.ps1
+++
@@ -1,3 +0,0 @@
-
-Get-AppxPackage -AllUsers | where-object {$_.name -notlike "*store*"} | Remove-AppxPackage
-reg add "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /f /ve
